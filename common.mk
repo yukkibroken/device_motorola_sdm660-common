6b39bf513cb59f95c082152a21c40e40847553c2
Marijn Suijten <marijn.suijten@somainline.org>
2021-10-08 18:55:50 +0200

    sdm660-common:Provide android.system.net.netd@1.1 on vendor for netmgr
    * starting with Android S most HALlibraries are not included in VNDK anymore
    - they need to be explicitly pulled in to `/vendor` for odm blobs to be allowed to access them:
    
        linker  : CANNOT LINK EXECUTABLE "/odm/bin/netmgrd": library "android.system.net.netd@1.1.so" not found: needed by main executable
    
    Change-Id: I5cf48d56d6d27fa4a27d3be4cac1c56111846038

--- common.mk
+++ common.mk
@@ -234,6 +234,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+# netmgrd
+PRODUCT_PACKAGES += \
+    android.system.net.netd@1.1.vendor
+
 # Network
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
